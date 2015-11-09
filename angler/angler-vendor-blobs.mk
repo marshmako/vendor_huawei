48ef0ba161482ad037e7ede97242eaf60e74c6c6
Sykopompos <sykopompos@gmail.com>
2015-11-08 21:38:48 -0500

    fix extra /

--- angler/angler-vendor-blobs.mk
+++ angler/angler-vendor-blobs.mk
@@ -118,4 +118,4 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/angler/proprietary/lib64/libloc_ds_api.so:system/lib64/libloc_ds_api.so \
     vendor/huawei/angler/proprietary/lib64/libloc_eng.so:system/lib64/libloc_eng.so \
     vendor/huawei/angler/proprietary/lib64/libManufacture.so:system/lib64/libManufacture.so \
-    vendor/huawei/angler/proprietary/xbin/wlutil:system/xbin/wlutil \
+    vendor/huawei/angler/proprietary/xbin/wlutil:system/xbin/wlutil 
